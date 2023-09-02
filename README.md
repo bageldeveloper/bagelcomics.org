1f8a8fbf6b7ab9577d2b1de4230aa6f939d3cae4
Starwarsdan3000 <106136427+Starwarsdan3000@users.noreply.github.com>
2023-09-02 10:11:33 -0700

    fun is math overhaul

--- README.md
+++
@@ -1,5 +0,0 @@
-Yet another dumb game website tee hee.
-
-I love you
-
-much better than original. hmmm yes. hmmm yes.
--- halflife/src/utils/setCanvasLoading.ts
+++
@@ -1,7 +0,0 @@
-const setCanvasLoading = () => {
-  const canvas = document.getElementById("canvas");
-  if (!canvas) return;
-  canvas.className += " " + "loading";
-};
-
-export default setCanvasLoading;
